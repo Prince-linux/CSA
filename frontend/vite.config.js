49ca6812c9fee7aa14614f237a78192a449e0dd4
Prince <regioths@gmail.com>
2025-08-14 11:39:40 +0000

    Final update with vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,28 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(),tailwindcss(),
+//   ],
+//   build: {
+//     target: 'esnext'
+//   }
+//   // base: '/gcap/usata/',
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
   build: {
-    target: 'esnext'
+    target: 'esnext',
+    outDir: 'dist', // ✅ Make sure Vercel knows where to find index.html
+    emptyOutDir: true
   }
-  // base: '/gcap/usata/',
 })
